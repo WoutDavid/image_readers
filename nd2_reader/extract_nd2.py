29e2fb26b8c37097bd9aa3cd1e3de3eae97d5f72
WoutDavid <wouters.david@hotmail.com>
2021-09-14 16:22:01 +0200

    cleaned up nd2 reader code

--- nd2_reader/extract_nd2.py
+++ nd2_reader/extract_nd2.py
@@ -2,11 +2,9 @@ import sys
 import os
 import argparse
 import numpy as np
-from icecream import ic
 import matplotlib.pyplot as plt
 from skimage import io
 from nd2reader import ND2Reader
-from nd2reader import parser
 
 
 ap = argparse.ArgumentParser(description="Extract specific slices from a raw .nd2 image.")
@@ -18,51 +16,36 @@ ap.add_argument('nd2_path',type=str,help="Path (relative or absolute) to target 
 ap.add_argument('-o', '--out_dir', type=str, help="Root directory where output should be stored, default is base dir of the input image")
 
 # Indices
-ap.add_argument('-c', '--c_numbers', default=-1, type=int, nargs='+', help="indexes (start at 0) of the channels that need to be extracted")
-ap.add_argument('-v', '--v_numbers', default=-1, type=int, nargs='+', help="indexes (start at 0) of the time series that need to be extracted")
-ap.add_argument('-z', '--z_numbers', default=-1, type=int, nargs='+', help="indexes (start at 0) of the z-stacks that need to be extracted.")
+ap.add_argument('-c', '--c_numbers', default=-1, type=int, nargs='+', help="Indexes (start at 0) of the channels that need to be extracted, default is all of them.")
+ap.add_argument('-v', '--v_numbers', default=-1, type=int, nargs='+', help="Indexes (start at 0) of the time series that need to be extracted, default is all of them.")
+ap.add_argument('-z', '--z_numbers', default=-1, type=int, nargs='+', help="Indexes (start at 0) of the z-stacks that need to be extracted, default is all of them.")
 
 # Booleans
-ap.add_argument('-m','--maxIP', default=False, action="store_true", help="Boolean to indicate whether to take a maxIP of the z-stacks, default is False")
 ap.add_argument('-r','--read', default=False, action="store_true", help="If true, the image will only be read and its dimensions will be printed out.")
+ap.add_argument('-m','--maxIP', default=False, action="store_true", help="Boolean to indicate whether to take a maxIP of the z-stacks, default is False")
 
 # parse args
 args = ap.parse_args()
 
-# now use arguments in any way you choose
-# show_math_result(args.a,args.b,args.op,args.to_upper)
-
-
-print(f"path =  {args.nd2_path}")
-
-nd2_filename_prefix = os.path.splitext(args.nd2_path)[0]
-print(f"prefix =  {nd2_filename_prefix}")
-
+# Extracht basename of the filepath to create future filenames
 nd2_filename_base = os.path.splitext(os.path.basename(args.nd2_path))[0]
-print(f"base =  {nd2_filename_base}")
 
+# If no output dir was input, use the base directory of the input file
 if not args.out_dir:
     args.out_dir =  os.path.dirname(args.nd2_path)
-print(f"output_dir = {args.out_dir}")
-
-
-print(f"maxip = {args.maxIP}")
-
 
 with ND2Reader(args.nd2_path) as images:
+    # If read flag, merely read it's dimensions and exit the script
     if args.read:
-        print(images.sizes)
+        print(f"{nd2_filename_base} dimensions: {images.sizes}")
         sys.exit()
+    # If no specific numbers were put in, default to the full range of the dimensions
     if args.z_numbers == -1:
         args.z_numbers = range(0,int(images.sizes['z']))
     if args.v_numbers == -1:
         args.v_numbers = range(0,int(images.sizes['v']))
     if args.c_numbers == -1:
         args.c_numbers = range(0,int(images.sizes['c']))
-
-    print(f"c_numbers = {args.c_numbers}")
-    print(f"v_numbers = {args.v_numbers}")
-    print(f"z_numbers = {args.z_numbers}")
 
     # first loop over channels
     for c_num in args.c_numbers:
@@ -83,5 +66,4 @@ with ND2Reader(args.nd2_path) as images:
             else:
                 for z_num in args.z_numbers:
                     temp_image = images.get_frame_2D(c=c_num, v=v_num, z=z_num)
-                    # ic(os.path.join(args.out_dir, nd2_filename_base, f"channel_{c_num+1}/",f"tile_{v_num+1}/",f"{nd2_filename_base}_extracted_c{c_num+1}_tile{v_num+1}_z{z_num+1}.tif"))
                     io.imsave(os.path.join(args.out_dir, nd2_filename_base, f"channel_{c_num+1}/",f"tile_{v_num+1}/",f"{nd2_filename_base}_extracted_c{c_num+1}_tile{v_num+1}_z{z_num+1}.tif"), temp_image)

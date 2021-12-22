55fd8271f61f89fbaf65e241e0d07d56bfc77310
WoutDavid <wouters.david@hotmail.com>
2021-12-22 15:41:32 +0100

    added function

--- nd2_reader/extract_ISS.py
+++ nd2_reader/extract_ISS.py
@@ -1,10 +1,11 @@
 import sys
 import os
 import numpy as np
+from glob import glob
 from skimage import io
 from nd2reader import ND2Reader
 
-def parseND2toISS(nd2_path: str, out_dir: str, round_nr: int, read = False, maxIP=True)
+def parseND2toISS(nd2_path: str, out_dir: str, round_nr: int, read = False, maxIP=True):
     # Extracht basename of the filepath to create future filenames
     nd2_filename_base = os.path.splitext(os.path.basename(nd2_path))[0]
 
@@ -20,9 +21,17 @@ def parseND2toISS(nd2_path: str, out_dir: str, round_nr: int, read = False, maxI
         if read:
             print(f"{nd2_filename_base} dimensions: {images.sizes}")
             sys.exit()
+
+        # Try is for when it only has one z stack, then it's not going to be a correct key
+        try:
+            z_numbers = range(0,int(images.sizes['z']))
+        except:
+            z_numbers =[0]
         # If no specific numbers were put in, default to the full range of the dimensions
-        z_numbers = range(0,int(images.sizes['z']))
-        v_numbers = range(0,int(images.sizes['v']))
+        try:
+            v_numbers = range(0,int(images.sizes['v']))
+        except:
+            v_numbers =[0]
         c_numbers = range(0,int(images.sizes['c']))
 
         # first loop over channels
@@ -41,3 +50,10 @@ def parseND2toISS(nd2_path: str, out_dir: str, round_nr: int, read = False, maxI
                     for z_num in z_numbers:
                         temp_image = images.get_frame_2D(c=c_num, v=v_num, z=z_num)
                         io.imsave(os.path.join(out_dir, f"{nd2_filename_base}_channel{c_num+1}_tile{v_num+1}_z{z_num+1}.tif"), temp_image)
+if __name__ == "__main__":
+    filenames = glob("/media/sda1/thesis_data/spatial2/raw/*.nd2")
+    out_dir = "/media/sda1/thesis_data/spatial2/raw/parsed/"
+    for i, file in enumerate(filenames):
+        parseND2toISS(file,out_dir, i, maxIP=False)
+
+    
